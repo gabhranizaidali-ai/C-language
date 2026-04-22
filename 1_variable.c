1587723e8baa9d8b531d3082d91388a74cdec231
gabhranizaidali-ai <gabhranizaidali@gmail.com>
2026-04-22 13:19:22 +0530

    Add files via upload

---
+++ 1_variable.c
@@ -0,0 +1,32 @@
+
+
+
+
+#include<stdio.h>
+
+int main()
+
+
+//  variable : which is store some data in our programing;
+
+// what is declaration;
+
+int num; 
+
+// what is variable initatization 
+
+num=24; 
+
+//  printf("%d,num")
+
+// reinitialization 
+
+num=48; 
+
+printf(%d,num")
+
+// you can't define variable with the same name again in same scope 
+
+num=50;
+
+// printf("%d,num")
